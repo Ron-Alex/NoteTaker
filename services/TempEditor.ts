e685d23ab404ae290038003c5905297d5977c4fa
Ron Alex <ronmathewalx@gmail.com>
2025-06-04 21:26:21 -0500

    Adding Temporary Editor functionality for loading Delta objects and rendering them as HTML. Added as service

---
+++ services/TempEditor.ts
@@ -0,0 +1,13 @@
+import { NoteObj } from "../models/NoteObj";
+
+export class TempEditor{
+        returnHTMLFromObj(content: NoteObj): any{
+        const tempDiv = document.createElement("div");
+        const tempEditor = new Quill(tempDiv, {
+            theme: 'snow'
+        });
+        tempEditor.setContents(content);
+        const HTMLData = tempEditor.getSemanticHTML();
+        return HTMLData;
+    }
+}
